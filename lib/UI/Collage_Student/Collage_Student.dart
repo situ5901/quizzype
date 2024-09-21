4e875a5e5233d668a02d4eea01480b5925284214
Situ-Kumar <er.situkumar@gmail.com>
2024-09-21 12:24:20 +0530

    update/21

---
+++ lib/UI/Collage_Student/Collage_Student.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(Collage_Student());
+}
+
+class Collage_Student extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Collage_Student',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue, // Set the app bar color to blue
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back,color: Colors.white,),
+            onPressed: () {
+              Navigator.of(context).pop(); // Navigate back when the button is pressed
+            },
+          ),
+        ),
+        body: Center(
+          child: SizedBox(
+            child: Lottie.asset('Assets/Images/search.json'),
+          ),
+        ),
+      ),
+    );
+  }
+}
